d03f775ead078b7af6640300ac8e62d148c9278b
ZaiZai <zyt75312@gmail.com>
2021-09-29 11:38:27 +0800

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jun3631 on 2021/9/29.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
